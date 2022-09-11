a65213fb05fcbc06c06d256339fc29bbc4a14a47
Dinesh kumar <reachtodinesh@gmail.com>
2022-09-11 07:51:04 +0530

    use variable shadowing

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -6,9 +6,9 @@ pub fn solve() {
     assert_eq!(117946, output, "part 1 solution should be 117946");
 
     //part 2
-    let part2_output = find_matching_md5(input, "000000");
-    println!("part2 {part2_output}");
-    assert_eq!(3938038, part2_output, "part 2 solution should be 3938038");
+    let output = find_matching_md5(input, "000000");
+    println!("part2 {output}");
+    assert_eq!(3938038, output, "part 2 solution should be 3938038");
 }
 
 fn find_matching_md5(input: &str, zero_trial: &str) -> usize {
